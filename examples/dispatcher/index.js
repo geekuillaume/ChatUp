99386d03af10b03bdb35eafaa2cece7320eb023b
Manish Valechha <manishval@hotmail.com>
2015-06-19 14:45:30 -0400

    Set dispatcher to listen to SSL connections

--- examples/dispatcher/index.js
+++ examples/dispatcher/index.js
@@ -1,5 +1,13 @@
 var ChatUp = require('../../index.js');
 var express = require('express');
+var https = require('https');
+var fs = require('fs');
+
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('/var/certs/streamup-key.pem'),
+  cert: fs.readFileSync('/var/certs/streamup-cert.cert')
+};
 
 var app = express();
 
@@ -10,3 +18,5 @@ dispatcher.register(app);
 app.listen(8000, function() {
   console.log('Dispatcher listening on port 8000');
 });
+
+https.createServer(options, app).listen(443);
